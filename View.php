44a52eaa7ca47cd74cc64f900892f4be77cf3016
sbrocos <sergiobrocos@gmail.com>
2012-10-29 18:28:46 +0100

    refactor view.php

--- View.php
+++ View.php
@@ -16,6 +16,70 @@ class IF_VIEW
 	 */
 	public function __construct( array $parametros = null, $layout )
 	{
+		$this->_gestionError( $parametros );
+
+		$this->_path = APP_PATH . '/main/views/' . $parametros['controller'];
+
+		$this->_gestionParams( $parametros );
+
+
+		if( !file_exists( $this->_path ) ){
+			$message = utf8_decode( "Carpeta e vistas (view) no encontrada" );
+			die( $message);
+		}
+
+		if( !file_exists( APP_PATH_LAYOUT . '/' . $layout ) ){
+			$message = utf8_decode( "No se ha encontrado el layout por defecto." );
+			die( $message);
+		} else {
+			$this->_layout = $layout;
+		}
+
+		$this->renderPHP();
+	}
+
+	/***
+	 *
+	 * @param array $parametros
+	 * @throws Exception
+	 */
+	public function renderPHP()
+	{
+		$controller = strtolower($this->_params['controller']);
+
+		include_once APP_PATH_LAYOUT. '/' . $this->_layout;
+
+
+	}
+
+	/**
+	 *
+	 */
+	public function contenido()
+	{
+		$action = strtolower( $this->_params['action'] );
+
+		extract( $this->_params['view_callback'] );
+
+		ob_start();
+
+		$path_view = $this->_path . "/$action.phtml";
+
+		include $path_view;
+
+		ob_end_flush();
+	}
+
+	protected function _gestionParams( array $parametros )
+	{
+		foreach ( $parametros as $key=>$val ) {
+			$this->_params[$key] = $val;
+		}
+
+	}
+
+	protected function _gestionError( array $parametros = null )
+	{
 		if( is_null( $parametros ) ) {
 			$error = "FATAL ERROR: no hay parametros para crear la vista";
 		} elseif ( !is_array( $parametros ) ) {
@@ -26,59 +90,10 @@ class IF_VIEW
 			$error = false;
 		}
 
-		if( !$error ){
-			$this->_params['contoller'] = $parametros['controller'];
-			$this->_params['action'] = $parametros['action'];
-			$this->_path = APP_PATH . '/main/views/' . $parametros['controller'];
-
-			if( !file_exists( $this->_path ) ){
-				$message = utf8_decode( "Carpeta e vistas (view) no encontrada" );
-				die( $message);
-			}
-
-			if( !file_exists( APP_PATH_LAYOUT . '/' . $layout ) ){
-				$message = utf8_decode( "No se ha encontrado el layout por defecto." );
-				die( $message);
-			} else {
-				$this->_layout = $layout;
-			}
-
-			$this->renderPHP( $parametros );
-		} else {
+		if( $error ) {
 			//TODO gestionar el error de manera pertinente.
 			throw new Exception( $error );
 			exit;
-
 		}
-	}
-
-	/***
-	 *
-	 * @param array $parametros
-	 * @throws Exception
-	 */
-	public function renderPHP( array $parametros = null )
-	{
-		$controller = strtolower($parametros['controller']);
-
-		include_once APP_PATH_LAYOUT. '/' . $this->_layout;
-
-		include_once APP_PATH_LAYOUT. '/' . $this->_layout;
-
-	}
-
-	/**
-	 *
-	 */
-	public function contenido()
-	{
-		$action = strtolower( $this->_params['action'] );
-		ob_start();
-
-		$path_view = $this->_path . "/$action.phtml";
-
-		include $path_view;
-
-		ob_end_flush();
 	}
 }
